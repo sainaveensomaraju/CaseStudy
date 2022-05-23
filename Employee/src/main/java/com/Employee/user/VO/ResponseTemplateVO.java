c04d2d3a51086bf5eff2f9f16e2fc329b8588507
sainveen <sainaveen2222@gmail.com>
2022-05-23 17:29:16 +0530

    save

---
+++ Employee/src/main/java/com/Employee/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,41 @@
+package com.Employee.user.VO;
+
+import com.Employee.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "ResponseTemplateVO [user=" + user + ", department=" + department + "]";
+	}
+	public ResponseTemplateVO(User user, Department department) {
+		super();
+		this.user = user;
+		this.department = department;
+	}
+	public ResponseTemplateVO() {
+		super();
+	}
+    
+}
